f2a1923d42e3dc5f24e66357acaf7009c0a09b66
Sushant Kumar <krsushant.sk@gmail.com>
2021-05-23 19:35:31 +0530

    Initiated the project

---
+++ app/src/main/java/com/sushant/quickbills/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sushant.quickbills
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

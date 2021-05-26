679ef59364746fb6a2dcea43d4ab234d0bdb1bc3
Sushant Kumar <krsushant.sk@gmail.com>
2021-05-26 09:20:14 +0530

    DashBoard UI Implemented

--- app/src/main/java/com/sushant/quickbills/MainActivity.kt
+++ app/src/main/java/com/sushant/quickbills/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sushant.quickbills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //TODO("Remember to change this function of login button")
+        submitBtn.setOnClickListener {
+            val myIntent = Intent(
+                this,
+                Dashboard::class.java
+            )
+            startActivity(myIntent)
+        }
     }
 }
